9abd1b74177c68a33086d8374e48418fc4abb90c
new-candidate <best.candidate@yandex.ru>
2020-09-03 14:36:13 +0300

    HW-1

--- app/src/main/java/ru/geekbrains/kotlin/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlin/MainActivity.kt
@@ -2,10 +2,29 @@ package ru.geekbrains.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.countButton)
+        button.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                viewModel.button()
+            }
+        })
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.getViewState().observe(this, Observer { value ->
+            Toast.makeText(this, value, Toast.LENGTH_SHORT).show()
+        })
     }
+
 }
